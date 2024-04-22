d98a6e37e31eb2df90981f69a3f2e80a7a542a29
Ritvikzz45 <65870026+Ritvikzz45@users.noreply.github.com>
2024-04-23 00:28:30 +0530

    Add files via upload

---
+++ BlogApplication_Sprint3/src/main/java/com/gl/blog/service/PostService.java
@@ -0,0 +1,16 @@
+package com.gl.blog.service;
+
+import com.gl.blog.payload.PostDto;
+import com.gl.blog.payload.PostResponse;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
